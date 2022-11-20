2751423d2fbc8e259c12d74ce855fa170ce98006
Viriato Nova <viriatosampaio@gmail.com>
2022-11-20 13:45:13 -0300

    transactions controller

--- backend/router.ts
+++ backend/router.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import { Register, Login, getUser, getUsers } from './src/Controllers/UserController';
+import { registerTransaction, getUserTransaction } from './src/Controllers/TransactionsController';
 import { Auth } from './src/Middlewares/Auth';
 
 export const route: Router = Router()
@@ -9,3 +10,5 @@ route.get('/api/v1/user', Auth, getUsers)
 route.get('/api/v1/user:username',Auth, getUser)
 route.post('/api/v1/register', Register);
 route.post('/api/v1/login', Auth, Login);
+route.get('/api/v1/transation:username', Auth, getUserTransaction);
+route.post('/api/v1/transation', Auth, registerTransaction);
